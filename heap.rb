a6f689fac72da8720667bcefc707f045f33dc1f6
Sean Yu <seansu4you87@gmail.com>
2014-10-18 23:57:33 -0700

    Heap!

---
+++ heap.rb
@@ -0,0 +1,112 @@
+class Heap
+  class << self
+    def heapify(array)
+    end
+
+    def merge(a, b)
+    end
+
+    def meld(a, b)
+    end
+  end
+  def initialize
+    @data = []
+    @size = 0
+  end
+
+  def peek
+    @data[1]
+  end
+
+  def extract
+    dom = @data[1]
+    @data[1] = @data[@size]
+    @data[@size] = nil
+    @data.pop if @data.last.nil?
+    @size -= 1
+
+    bubble_down(1)
+    dom
+  end
+
+  def insert(key)
+    # append to the end of the array, bubble up
+    @data[@size + 1] = key
+    @size += 1
+    bubble_up(@size)
+  end
+
+  def size
+    @size
+  end
+
+  def empty?
+    @size == 0
+  end
+
+  def to_s
+    _, *tail = @data
+    tail.join(', ')
+  end
+
+  private
+
+  def dominates?(a, b)
+    a < b # min heap domination
+  end
+
+  def dominant(a, b)
+    return [a, :left] if b.nil?
+    return [b, :right] if a.nil?
+
+    if dominates?(a, b)
+      [a, :left]
+    else
+      [b, :right]
+    end
+  end
+
+  def bubble_up(index)
+    return if parent(index) == -1
+
+    if dominates?(@data[index], @data[parent(index)])
+      tmp = @data[parent(index)]
+      @data[parent(index)] = @data[index]
+      @data[index] = tmp
+      bubble_up(parent(index))
+    end
+  end
+
+  def bubble_down(index)
+    return if index >= @size
+
+    left = @data[left_child(index)]
+    right = @data[right_child(index)]
+    dom, side = dominant(left, right)
+
+    if dominates?(dom, @data[index])
+      tmp = @data[index]
+      @data[index] = dom
+      if side == :left
+        @data[left_child(index)] = tmp
+        bubble_down(left_child(index))
+      else
+        @data[right_child(index)] = tmp
+        bubble_down(right_child(index))
+      end
+    end
+  end
+
+  def parent(n)
+    return -1 if n == 1
+    (n / 2).floor
+  end
+
+  def left_child(n)
+    2 * n
+  end
+
+  def right_child(n)
+    left_child(n) + 1
+  end
+end
---
+++ heap_spec.rb
@@ -0,0 +1,30 @@
+require_relative "heap"
+
+describe Heap do
+  describe '#insert' do
+    it "inserts" do
+      h = Heap.new
+      h.insert 5  # 5
+      h.insert 3  # 5, 3 -> 3, 5
+      h.insert 4  # 3, 5, 4
+      h.insert 20 # 3, 5, 4, 20
+      h.insert 1  # 3, 5, 4, 20, 1 -> 3, 1, 4, 20, 5 -> 1, 3, 4, 20, 5
+      expect(h.to_s).to eql "1, 3, 4, 20, 5"
+    end
+  end
+
+  describe "#extract" do
+    it "extracts the most dominant member" do
+      h = Heap.new
+      h.insert 5
+      h.insert 3
+      h.insert 4
+      h.insert 20
+      h.insert 1  # 1, 3, 4, 20, 5
+      h.insert 36 # 1, 3, 4, 20, 5, 36
+
+      expect(h.extract).to eql 1
+      expect(h.to_s).to eql "3, 5, 4, 20, 36"
+    end
+  end
+end
